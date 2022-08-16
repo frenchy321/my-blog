e445eb4629134c8b21de1468099b5e520416ca72
Andy <andy.french@hotmail.co.uk>
2022-08-16 10:24:17 +0100

    Used setTimeOut to fetch data and throw error

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
@@ -6,7 +6,9 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2 className="">{blog.title}</h2>
           <p>written by {blog.author}</p>
-          <button className="btn btn-md bg-primary text-white" onClick={() => handleDelete(blog.id)}>Click</button>
+          <button className="blog-list-btn btn btn-md bg-primary text-white">
+            Click
+          </button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -2,35 +2,34 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "tip end", body: "lorem10", author: "me", id: 1 },
-    { title: "tip top", body: "lorem11", author: "me", id: 2 },
-    { title: "tip shop", body: "lorem12", author: "shell", id: 3 },
-    { title: "tip bop", body: "lorem13", author: "lee", id: 4 }
-  ]);
-
-  const [name, setName] = useState("shell");
-
-  const handleDelete = id => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    console.log("use effect ran");
-    console.log(name);
-  }, [name]);
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then(res => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for that resource!");
+          }
+        })
+        .then(data => {
+          setBlogs(data);
+          setIsLoading(false);
+          setError(false);
+        })
+        .catch(err => {
+          setIsLoading(false);
+        });
+    }, 1000);
+  }, []);
 
   return (
     <div className="home p-5">
-      <BlogList blogs={blogs} title="All Blogs!!" handleDelete={handleDelete} />
-      <button
-        onClick={() => setName("luigi")}
-        className="mt-5 btn btn-danger btn-md text-white"
-      >
-        change name
-      </button>
-      <p>{name}</p>
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
   );
 };
